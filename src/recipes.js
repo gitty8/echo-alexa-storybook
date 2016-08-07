7c9afde35e5cbcb30108e800ce5ff9c85d458589
mradambeck <mr.adambeck@gmail.com>
2016-08-07 12:03:28 -0700

    proof of concept complete

---
+++ src/recipes.js
@@ -0,0 +1,24 @@
+/**
+    Copyright 2014-2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+    Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
+
+        http://aws.amazon.com/apache2.0/
+
+    or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+*/
+
+// 'Alexa, play a song',
+// 'Alexa, read a story',
+// 'Alexa, play something educational'
+
+
+module.exports = {
+
+    "song": '<speak><audio src="https://s3.amazonaws.com/story-audio/down+by+the+station.mp3" /></speak>',
+    "educational": '<speak><audio src="https://s3.amazonaws.com/story-audio/ABC.mp3" /></speak>',
+    "learning": '<speak><audio src="https://s3.amazonaws.com/story-audio/ABC.mp3" /></speak>',
+    "nursery rhyme": '<speak><audio src="https://s3.amazonaws.com/story-audio/ABC.mp3" /></speak>',
+    "story": '<speak><audio src="https://s3.amazonaws.com/story-audio/huck+finnn.mp3" /></speak>'
+
+};
